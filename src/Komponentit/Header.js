2ddd54bf9badba8a3271dc64996026c0c2ed0917
PennasenKake <121464958+PennasenKake@users.noreply.github.com>
2024-03-03 00:22:20 +0200

    Muokattu headerin logosta pääsee etusivulle

--- src/Komponentit/Header.js
+++ src/Komponentit/Header.js
@@ -1,21 +1,16 @@
-// Header.js
 import React from "react";
+import { Link } from 'react-router-dom'; // Lisätty Link
 import { Container, Row, Col } from 'react-bootstrap';
-import '../App.css'; // Tuodaan tyylitiedosto
+import '../App.css';
 
-// Header-komponentti
 const Header = () => {
-  
-  // Funktio taustakuvan valitsemiseksi näytön koon perusteella
   const getBackgroundImage = () => {
-    // Tarkistetaan näytön koko ja valitaan taustakuva sen mukaan
     const isSmallScreen = window.innerWidth <= 768 && window.innerHeight <= 831;
     return isSmallScreen
       ? `url(${process.env.PUBLIC_URL}/Media/mahdollinen_tausta1.jpg)`
       : `url(${process.env.PUBLIC_URL}/Media/mahdollinen_tausta2.jpg)`;
   };
 
-  // JSX-rakenne
   return (
     <div className="header-section" 
       style={{ 
@@ -25,16 +20,17 @@ const Header = () => {
       }}>
       <Container>
         <Row className="align-items-center">
-          {/* Logo-osio */}
+          {/* Lisätty Link-komponentti ympärille */}
           <Col lg={3} md={12} className="text-lg-end">
-            <img src={`${process.env.PUBLIC_URL}/Media/kirsujen_logo2.svg`} 
-            alt="Kivat Kirsut Logo" className="logo" />
+            <Link to="/">
+              <img src={`${process.env.PUBLIC_URL}/Media/kirsujen_logo2.svg`} 
+                alt="Kivat Kirsut Logo" className="logo" />
+            </Link>
           </Col>
-          {/* Otsikon osio */}
           <Col lg={6} md={12} className="text-center">
             <h1 className="header-title"
-            style={{ color: 'white', textShadow: '2px 2px 4px #000000',}}>
-            NILSIÄN KIVAT KIRSUT RY</h1>
+              style={{ color: 'white', textShadow: '2px 2px 4px #000000',}}>
+              NILSIÄN KIVAT KIRSUT RY</h1>
           </Col>
         </Row>
       </Container>
